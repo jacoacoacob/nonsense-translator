24bddf88f93f36293a54bfe0058721942223c343
jacoacoacob <jacoacoacob@protonmail.com>
2024-02-11 16:34:01 -0600

    v2 translator

--- arp-lang.js
+++ arp-lang.js
@@ -41,8 +41,6 @@ function isSilent(match, offset, source) {
  * @param {string} text 
  */
 function translate(text) {
-  // const visited = [];
-
   return text
     .split(" ")
     .map((word) =>
@@ -57,4 +55,93 @@ function translate(text) {
     .join(" ");
 }
 
-export { translate };
+
+
+
+/**
+ * 
+ * @param {string} text 
+ */
+function translateV2(text) {
+  const visited = {};
+
+  function visit(wordIndex, match, offset) {
+    if (!visited[wordIndex]) {
+      visited[wordIndex] = [];
+    }
+    visited[wordIndex].push({
+      start: offset,
+      end: offset + match.length,
+      repaced: false,
+    });
+  }
+
+  function isVisited(wordIndex, offset) {
+    return visited[wordIndex] && visited[wordIndex].some(
+      (x) => x.start <= offset && x.end >= offset
+    );
+  }
+
+  function isSilentE(match, offset, source) {
+    return (
+      offset >= source.length - 2 &&
+      /es?$/i.test(source) &&
+      !WITH_AUDIBLE_ENDING_E.includes(source)
+    );
+  }
+
+  function firstReplacer(word, wordIndex) {
+    const processed = word.replace(
+      /ou|ee|ea|ay|ey|oo|io|oi|oy|oa|ui|ua/ig,
+      (match, offset, _source) => {
+        visit(wordIndex, match, offset);
+
+        console.log("[firstReplacer]", match, offset)
+
+        return `${isCaps(match[0]) ? "A" : "a"}rp${match}`;
+      }
+    );
+
+    if (visited[wordIndex]) {
+      visited[wordIndex].forEach((item) => {
+        item.end += 3;
+      });
+    }
+
+    return processed;
+  }
+
+  function secondReplacer(word, wordIndex) {
+    return word.replace(/[aeiou]|y$/ig, (match, offset, source) => {
+          
+      console.log("[second round]", match, offset, source);
+      if (isVisited(wordIndex, offset)) {
+        return match;
+      }
+
+      visit(wordIndex, match, offset);
+
+      if (isSilentE(match, offset, source)) {
+        return match;
+      }
+
+      return `${isCaps(match) ? "A" : "a"}rp${match}`;
+    });
+  }
+
+  function processWord(word, wordIndex, ...fns) {
+    return fns.reduce((accum, fn) => fn(accum, wordIndex), word.trim());
+  }
+
+  return text
+    .split(" ")
+    .map((word, wordIndex) => processWord(
+      word,
+      wordIndex,
+      firstReplacer,
+      secondReplacer
+    ))
+    .join(" ");
+}
+
+export { translate, translateV2 };
--- main.js
+++ main.js
@@ -2,20 +2,38 @@ import * as arpLang from "./arp-lang.js";
 
 const contentWrapper = document.querySelector("#content-wrapper");
 const ta = document.querySelector("#input");
-const cardInput = document.querySelector("#input-card");
-const cardOutput = document.querySelector("#output-card");
+const inputCard = document.querySelector("#input-card");
+const outputCard = document.querySelector("#output-card");
+const output = document.querySelector("#output");
+const selectTranslateFn = document.querySelector("#select-translate-fn");
 
 ta.focus();
 autoSizeElementHeight(ta);
 autoSizeElementHeight(contentWrapper);
+doTranslation(ta.value);
 
 ta.addEventListener("input", (ev) => {
   autoSizeElementHeight(ta);
   autoSizeElementHeight(contentWrapper);
-
-  const arp = arpLang.translate(ev.target.value);
-  cardOutput.textContent = arp;
+  doTranslation(ev.target.value);
 });
+
+inputCard.addEventListener("click", (_ev) => {
+  ta.focus();
+});
+
+selectTranslateFn.addEventListener("change", onChangeSelectTranslateFn);
+
+function doTranslation(text) {
+  if (selectTranslateFn.value === "v2") {
+    const arp = arpLang.translateV2(text);
+    output.textContent = arp;
+  }
+  if (selectTranslateFn.value === "v1") {
+    const arp = arpLang.translate(text);
+    output.textContent = arp;
+  }
+}
 
 /**
  * 
@@ -26,3 +44,7 @@ function autoSizeElementHeight(el, minHeight = 0) {
   el.style.height = 0;
   el.style.height = Math.max(el.scrollHeight, minHeight) + 10 + "px";
 }
+
+function onChangeSelectTranslateFn(ev) {
+  doTranslation(ta.value);
+} 
